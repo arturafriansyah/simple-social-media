e0ae8ca17d799ecddc489cce9b93b89639315df9
arturafriansyah <42965691+arturafriansyah@users.noreply.github.com>
2025-12-10 11:17:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
-npm install
-npm run dev
+#!/bin/sh
+set -e
+
+# Install dependency composer
+composer install --no-dev --optimize-autoloader
+
+# Install dependency Node.js
+npm install --legacy-peer-deps
+
+# Build asset Laravel Mix untuk production
+npm run production
+
+# Setup file environment
 cp .env.example .env
+
 php artisan key:generate
 
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.1/g' .env &&
-sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
+# Update konfigurasi database
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.1/g' .env
+sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env
 
-php artisan migrate
-php artisan db:seed
+php artisan migrate --force
+php artisan db:seed --force
